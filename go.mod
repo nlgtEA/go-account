e5375371f6356688b33b380c7e38533b70624dcc
thuan.ngo <thuan.ngo@eastagile.com>
2024-01-27 22:42:48 +0700

    templ up and running

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/nlgtEA/go-account
 
 go 1.21.0
 
-require (
-	github.com/go-chi/chi v1.5.5
-	github.com/go-chi/chi/v5 v5.0.11
-)
+require github.com/go-chi/chi/v5 v5.0.11
+
+require github.com/a-h/templ v0.2.543
--- handler/user.go
+++ handler/user.go
@@ -1,9 +1,15 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/nlgtEA/go-account/view/user"
+)
 
 type UserHandler struct{}
 
 func (uh *UserHandler) HandlerUserShow(w http.ResponseWriter, r *http.Request) {
+	page := user.Hello()
+	page.Render(r.Context(), w)
 	w.Write([]byte("U ri sarang haess channa"))
 }
